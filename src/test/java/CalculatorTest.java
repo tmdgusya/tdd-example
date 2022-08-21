b5d5437d90552e5b3657b79e4e73f1db56793040
tmdgusya <tmdgusya@naver.com>
2022-08-21 19:39:52 +0900

    Calculator Plus method 추가

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    void plus() {
+        int result = Calculator.plus(1, 2);
+        assertEquals(3, result);
+        assertEquals(5, Calculator.plus(1, 4));
+    }
+
+}
